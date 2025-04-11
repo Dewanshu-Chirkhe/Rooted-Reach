460281b11b9e5796a1c7524771f11988dbaee6d0
Dewanshu-Chirkhe <dewanshuchirkhe2004@gmail.com>
2025-04-11 09:13:11 +0530

    added background image locally

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,8 +8,7 @@ const Hero = () => {
             <div
                 className="absolute inset-0 w-full h-full"
                 style={{
-                    backgroundImage:
-                        "url('https://sdmntpreastus2.oaiusercontent.com/files/00000000-28f4-61f6-a985-279e23f7842c/raw?se=2025-04-11T03%3A27%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=7fed5391-247d-5f05-8111-11d67d7142ad&skoid=2f36945c-3adc-4614-ac2b-eced8f672c58&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-10T14%3A22%3A32Z&ske=2025-04-11T14%3A22%3A32Z&sks=b&skv=2024-08-04&sig=BdUUUo7yRCPN70PBg3KjMQUm0lBas9usQZS3sbYWZg4%3D')",
+                    backgroundImage: "url('/images/BGimage.png')",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     backgroundRepeat: "no-repeat",
